b48f262e8a249cdab6981d1a4046209289ca6ed2
Lucas Teixeira <lucasgayosot@gmail.com>
2024-04-11 17:53:38 -0300

    pagina index

---
+++ Raíz/js/scripts.js
@@ -0,0 +1,15 @@
+// Pagina Index botoes 
+
+// Adiciona event listener para o botão de entrar 
+document.getElementById('btn-entrar').addEventListener('click', function() {
+    // Redireciona para a página desejada
+    window.location.href = 'login.html'; // Substitua 'url_da_outra_pagina.html' pela URL da página para onde deseja redirecionar
+});
+
+
+// Adiciona event listener para o botão de cadastrar
+document.getElementById('btn-cadastrar').addEventListener('click', function() {
+    // Redireciona para a página desejada
+    window.location.href = 'cadastro.html'; // Substitua 'url_da_outra_pagina.html' pela URL da página para onde deseja redirecionar
+});
+
