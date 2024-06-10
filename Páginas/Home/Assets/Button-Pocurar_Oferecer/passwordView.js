15eccba1fbfcd35a13a058db2d00ca6625e78673
CarlosCampos <carloscampos.bn@gmail.com>
2024-06-10 14:58:49 -0300

    uPDATE

--- Páginas/Home/Assets/Button-Pocurar_Oferecer/passwordView.js
+++ Páginas/Home/Assets/Button-Pocurar_Oferecer/passwordView.js
@@ -1,14 +1,14 @@
 
 function showing(){
-    var inputPass=document.getElementById('senha');
-    var btnShowPass = document.getElementById('btnSenha');
+    var inputPass=document.getElementById("senha");
+    var btnShowPass = document.getElementById("btnSenha");
 
-    if(inputPass.type === 'password'){
-        inputPass.setAttribute('type' , 'text');
-        btnShowPass.classList.replace ('fa-eye' , 'fa-eye-slash')
+    if(inputPass.type === "password"){
+        inputPass.setAttribute("type" , "text");
+        btnShowPass.classList.replace ("fa-eye" , "fa-eye-slash")
     }else{
-        inputPass.setAttribute('type' , 'password');
-        btnShowPass.classList.replace ('fa-eye-slash','fa-eye')
+        inputPass.setAttribute("type" , "password");
+        btnShowPass.classList.replace ("fa-eye-slash","fa-eye")
     }
 
 }
@@ -18,15 +18,15 @@ function showing(){
 
 // Olhos icone senha confirm
 function showingConfirm(){
-    var inputPassConfirm=document.getElementById('confirma-senha');
-    var btnShowPassConfirm = document.getElementById('btnSenha2');
+    var inputPassConfirm=document.getElementById("confirmaSenha");
+    var btnShowPassConfirm = document.getElementById("btnSenha2");
 
-    if(inputPassConfirm.type === 'password'){
-        inputPassConfirm.setAttribute('type' , 'text');
-        btnShowPassConfirm.classList.replace ('fa-eye' , 'fa-eye-slash' )
+    if(inputPassConfirm.type === "password"){
+        inputPassConfirm.setAttribute("type" , "text");
+        btnShowPassConfirm.classList.replace ("fa-eye" , "fa-eye-slash")
     }else{
-        inputPassConfirm.setAttribute('type' , 'password');
-        btnShowPassConfirm.classList.replace ('fa-eye-slash','fa-eye')
+        inputPassConfirm.setAttribute("type" , "password");
+        btnShowPassConfirm.classList.replace ("fa-eye-slash","fa-eye")
     }
 
 }
